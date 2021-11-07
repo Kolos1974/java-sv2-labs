ee5f53b996ea804b19ed38cf51a1e73d72909785
Kolos1974 <karlovitz.kolos@gmail.com>
2021-11-07 13:29:52 +0100

    introjunit tasks done

---
+++ src/test/java/GentlemanTest.java
@@ -0,0 +1,28 @@
+import introjunit.Gentleman;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class GentlemanTest {
+
+    @Test
+    public void testCase(){
+        // Given
+        Gentleman gentleman = new Gentleman();
+
+        // When
+        String name = gentleman.sayHello("John Doe");
+
+        // Then
+        assertThat(name, equalTo("Hello John Doe!"));
+    }
+
+    @Test
+    public void testCaseInLine(){
+        assertThat(new Gentleman().sayHello("John Doe"), equalTo("Hello John Doe!"));
+        assertThat(new Gentleman().sayHello(null), equalTo("Hello Anonymous!"));
+
+    }
+
+}
