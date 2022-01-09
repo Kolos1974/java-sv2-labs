b7dc39165e1687035122a16e15294fc509055c72
Kolos1974 <karlovitz.kolos@gmail.com>
2022-01-10 00:27:51 +0100

    some test passed

--- src/main/java/projects/catalog/AudioFeatures.java
+++ src/main/java/projects/catalog/AudioFeatures.java
@@ -1,21 +1,28 @@
 package projects.catalog;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class AudioFeatures implements Feature{
+public class AudioFeatures implements Feature {
     private List<String> composer;
     private int length;
     private List<String> performers;
     private String title;
 
     public AudioFeatures(String title, int length, List<String> composer) {
+        if (Validators.isBlank(title)){
+            throw new IllegalArgumentException("Title is empty!");
+        }
+        if (length<=0){
+            throw new IllegalArgumentException("Length is wrong value");
+        }
         this.title = title;
         this.length = length;
         this.composer = composer;
 
     }
 
-    public AudioFeatures(String title, int length, List<String> composer, List<String> performers) {
+    public AudioFeatures(String title, int length, List<String> performers, List<String> composer) {
         this.title = title;
         this.length = length;
         this.composer = composer;
@@ -25,12 +32,19 @@ public class AudioFeatures implements Feature{
 
     @Override
     public List<String> getContributors() {
-        return null;
+        List<String> result = new ArrayList<>();
+        if (!(Validators.isEmpty(composer))) {
+            result.addAll(composer);
+        }
+        if (!(Validators.isEmpty(performers))) {
+            result.addAll(performers);
+        }
+        return result;
     }
 
     @Override
     public String getTitle() {
-        return null;
+        return title;
     }
 
     public int getLength() {
--- src/main/java/projects/catalog/PrintedFeatures.java
+++ src/main/java/projects/catalog/PrintedFeatures.java
@@ -8,6 +8,15 @@ public class PrintedFeatures implements Feature {
     private String title;
 
     public PrintedFeatures(String title, int numberOfPages, List<String> authors) {
+        if (Validators.isBlank(title)){
+            throw new IllegalArgumentException("Empty title");
+        }
+        if (numberOfPages<=0){
+            throw new IllegalArgumentException();
+        }
+        if (Validators.isEmpty(authors)){
+            throw new IllegalArgumentException();
+        }
         this.title = title;
         this.numberOfPages = numberOfPages;
         this.authors = authors;
