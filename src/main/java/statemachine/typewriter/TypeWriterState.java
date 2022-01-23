6f416c57fea068173b9016a3e13ba6c6f87b3245
Kolos1974 <karlovitz.kolos@gmail.com>
2022-01-23 19:38:24 +0100

    statemachine tasks done

---
+++ src/main/java/statemachine/typewriter/TypeWriterState.java
@@ -0,0 +1,18 @@
+package statemachine.typewriter;
+
+public enum TypeWriterState {
+    UPPERCASE{
+        @Override
+        public TypeWriterState next() {
+            return TypeWriterState.LOWERCASE;
+        }
+    },
+    LOWERCASE{
+        @Override
+        public TypeWriterState next() {
+            return TypeWriterState.UPPERCASE;
+        }
+    };
+
+    public abstract TypeWriterState next();
+}
